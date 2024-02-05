0245c32d8d0802d789414846a999edf48068fc76
dpksi <dpksi@DESKTOP-HIU8CH7>
2024-02-05 11:13:12 +0530

    New_Test

---
+++ java_programs/src/nbhgfhj_jh/newtest.java
@@ -0,0 +1,14 @@
+package nbhgfhj_jh;
+
+public class newtest {
+
+	public newtest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
